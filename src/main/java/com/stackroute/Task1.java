139d3a66757f2bcc39278d1008308d36564b3813
shivanisajjan <shivanisajjan269@gmail.com>
2019-09-25 09:49:46 +0530

    sixth commit

--- src/main/java/com/stackroute/Task1.java
+++ src/main/java/com/stackroute/Task1.java
@@ -7,6 +7,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Task1 {
     public static void main(String[] args) {
         AbstractApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        BeanLifecycleDemoBean b=context.getBean("beanlifecycle",BeanLifecycleDemoBean.class);
+        b.display();
+        BeanLifecycleDemoBean b1=context.getBean("beanlifecycle1",BeanLifecycleDemoBean.class);
+        b1.display();
         context.registerShutdownHook();
     }
 }
